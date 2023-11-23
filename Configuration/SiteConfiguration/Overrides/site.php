a25f156d7a6804b8d2fcd557bf327ddbe17aa3da
Elias Häußler <e.haeussler@familie-redlich.de>
2023-11-23 11:50:16 +0100

    [BUGFIX] Make site configuration overrides TYPO3 v11 compatible

--- Configuration/SiteConfiguration/Overrides/site.php
+++ Configuration/SiteConfiguration/Overrides/site.php
@@ -19,20 +19,29 @@
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
-$GLOBALS['SiteConfiguration']['site']['columns']['sitemap_robots_inject'] = [
-    'label' => 'LLL:EXT:sitemap_robots/Resources/Private/Language/locallang_db.xlf:site.sitemap_robots_inject.label',
-    'description' => 'LLL:EXT:sitemap_robots/Resources/Private/Language/locallang_db.xlf:site.sitemap_robots_inject.description',
-    'config' => [
-        'type' => 'check',
-        'renderType' => 'checkboxLabeledToggle',
-        'items' => [
-            [
-                'label' => '',
-                'labelChecked' => 'LLL:EXT:sitemap_robots/Resources/Private/Language/locallang_db.xlf:site.sitemap_robots_inject.item.checked',
-                'labelUnchecked' => 'LLL:EXT:sitemap_robots/Resources/Private/Language/locallang_db.xlf:site.sitemap_robots_inject.item.unchecked',
+(static function () {
+    if ((new \TYPO3\CMS\Core\Information\Typo3Version())->getMajorVersion() >= 12) {
+        $labelKey = 'label';
+    } else {
+        // @todo Remove once support for TYPO3 v11 is dropped
+        $labelKey = 0;
+    }
+
+    $GLOBALS['SiteConfiguration']['site']['columns']['sitemap_robots_inject'] = [
+        'label' => 'LLL:EXT:sitemap_robots/Resources/Private/Language/locallang_db.xlf:site.sitemap_robots_inject.label',
+        'description' => 'LLL:EXT:sitemap_robots/Resources/Private/Language/locallang_db.xlf:site.sitemap_robots_inject.description',
+        'config' => [
+            'type' => 'check',
+            'renderType' => 'checkboxLabeledToggle',
+            'items' => [
+                [
+                    $labelKey => '',
+                    'labelChecked' => 'LLL:EXT:sitemap_robots/Resources/Private/Language/locallang_db.xlf:site.sitemap_robots_inject.item.checked',
+                    'labelUnchecked' => 'LLL:EXT:sitemap_robots/Resources/Private/Language/locallang_db.xlf:site.sitemap_robots_inject.item.unchecked',
+                ],
             ],
         ],
-    ],
-];
+    ];
 
-$GLOBALS['SiteConfiguration']['site']['palettes']['xml_sitemap']['showitem'] .= ', --linebreak--, sitemap_robots_inject';
+    $GLOBALS['SiteConfiguration']['site']['palettes']['xml_sitemap']['showitem'] .= ', --linebreak--, sitemap_robots_inject';
+})();
